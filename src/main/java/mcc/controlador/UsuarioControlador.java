8bf5b53f7ded842d4262efde957ceb7afbd7c17c
Heriberto G <heriberto.galdamez@gmail.com>
2015-05-28 23:31:26 -0500

    Se quita runtime de myeclipse, se agregan librerias manualmente, se agrega primera pagina de spring mvc de prueba

--- src/main/java/mcc/controlador/UsuarioControlador.java
+++ src/main/java/mcc/controlador/UsuarioControlador.java
@@ -1,4 +1,13 @@
 package mcc.controlador;
+
+import mcc.data.RolDAO;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Clase de controlador
  * que atiende las peticiones
@@ -6,6 +15,26 @@ package mcc.controlador;
  * 
  *
  */
+
+
+@Controller
+@RequestMapping("usuario")
 public class UsuarioControlador {
+	
+	private static final Logger log = Logger.getLogger(UsuarioControlador.class);
+	
+	@Autowired
+	private RolDAO rolDAO;
+	
+	
+	
+	@RequestMapping("listaUsuarios")
+	public ModelAndView listarUsuarios(){
+		log.debug("Entrando a listarUsuarios");
+		return new ModelAndView("usuario/listaUsuario","listaRoles",rolDAO.findAll());
+	}
+	
+	
+
 
 }
